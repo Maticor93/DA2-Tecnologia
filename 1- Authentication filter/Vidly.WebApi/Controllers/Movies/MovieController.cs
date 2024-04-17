91e8431661080683a7a6018f9ab0d18127f27815
Daniel Acevedo <149620005+danielAcevedo18@users.noreply.github.com>
2024-04-17 10:27:30 -0300

    Refactor

--- 1- Authentication filter/Vidly.WebApi/Controllers/Movies/MovieController.cs
+++ 1- Authentication filter/Vidly.WebApi/Controllers/Movies/MovieController.cs
@@ -18,101 +18,11 @@ namespace Vidly.WebApi.Controllers.Movies
         }
 
         [HttpPost]
-        public CreateMovieResponse Create([FromBody]CreateMovieRequest? newMovie)
+        public void Create([FromBody]CreateMovieRequest? newMovie)
         {
             var userLogged = base.GetUserLogged();
 
-            var hasNotPermission = userLogged.Role.HasPermission(PermissionKey.CreateMovie);
-            if (hasNotPermission)
-                ThrowException("Forbidden", $"Missing permission {PermissionKey.CreateMovie}");
-
-            if (newMovie == null)
-                ThrowException("InvalidRequest", "Request can not be null");
-
-            if (string.IsNullOrEmpty(newMovie.Title))
-                ThrowException("InvalidRequest", "Title can not be null or empty");
-
-            if (string.IsNullOrEmpty(newMovie.Description))
-                ThrowException("InvalidRequest", "Description can not be null or empty");
-
-            if (newMovie.PublishedOn == null)
-                ThrowException("InvalidRequest", "PublishedOn can not be null");
-
-            var existMovie = _movies.Any(m => m.Title == newMovie.Title);
-            if (existMovie)
-                ThrowException("DuplicatedResource", $"Movie with prop: Title and value: {newMovie.Title} exist");
-
-            var movieToSave = new Movie(
-              newMovie.Title,
-              newMovie.Description,
-              DateTimeOffset.Parse(newMovie.PublishedOn));
-
-            _movies.Add(movieToSave);
-
-            return new CreateMovieResponse(movieToSave);
+            // rest of code
         }
-
-        [HttpGet]
-        public List<MovieBasicInfoResponse> GetAll([FromQuery] MovieFiltersRequest filters)
-        {
-            return _movies
-                .Where(m =>
-                (string.IsNullOrEmpty(filters.Title) || m.Title.Contains(filters.Title)) &&
-                (!filters.MinStars.HasValue || m.Stars >= filters.MinStars) &&
-                (string.IsNullOrEmpty(filters.PublishedOn) || m.PublishedOn == DateTimeOffset.Parse(filters.PublishedOn)))
-                .Select(m => new MovieBasicInfoResponse(m))
-                .ToList();
-        }
-
-        [HttpGet("{id}")]
-        public MovieDetailInfoResponse GetById(string id)
-        {
-            var isValidId = Guid.TryParse(id, out var movieId);
-            if (!isValidId)
-                ThrowException("InvalidArgument", "The provided id is not a valid guid");
-
-            var movie = _movies.FirstOrDefault(m => m.Id == id);
-
-            if (movie == null)
-                ThrowException("ResourceNotFound", $"Movie with id: {id} does not exist");
-
-            return new MovieDetailInfoResponse(movie);
-        }
-
-        [HttpDelete("{id}")]
-        public void DeleteById(string id)
-        {
-            var isValidId = Guid.TryParse(id, out var movieId);
-            if (!isValidId)
-                ThrowException("InvalidArgument", "The provided id is not a valid guid");
-
-            var movie = _movies.FirstOrDefault(m => m.Id == id);
-
-            if (movie == null)
-                ThrowException("ResourceNotFound", $"Movie with id: {id} does not exist");
-
-            _movies.Remove(movie);
-        }
-
-        [HttpPut("{id}")]
-        public void UpdateById(string id, UpdateMovieRequest? updatesOfMovie)
-        {
-            if (updatesOfMovie == null)
-                ThrowException("InvalidRequest", "The request can not be null");
-
-            var isValidId = Guid.TryParse(id, out var movieId);
-            if (!isValidId)
-                ThrowException("InvalidArgument", "The provided id is not a valid guid");
-
-            var movie = _movies.FirstOrDefault(m => m.Id == id);
-
-            if (movie == null)
-                ThrowException("ResourceNotFound", $"Movie with id: {id} does not exist");
-
-            if (!string.IsNullOrEmpty(updatesOfMovie.Description))
-                movie.Description = updatesOfMovie.Description;
-        }
-
-        private static void ThrowException(string code, string description) => throw new Exception($"Code:{code}, Description: {description}");
     }
 }
