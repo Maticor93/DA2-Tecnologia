56cc8a358109234ebcd3b66f5cdee4c494fd3445
Daniel Acevedo <daniel18acevedo@gmail.com>
2024-04-21 01:00:02 -0300

    Refactor

--- 3- Unit test Repository/Vidly.WebApi/Controllers/Movies/Models/UpdateMovieRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Vidly.WebApi.Controllers.Movies.Models
-{
-    public sealed record class UpdateMovieRequest
-    {
-        public string? Description { get; init; }
-    }
-}
--- 3- Unit test Repository/Vidly.WebApi/Program.cs
+++ 3- Unit test Repository/Vidly.WebApi/Program.cs
@@ -1,9 +1,4 @@
-using Microsoft.EntityFrameworkCore;
 using System.Diagnostics.CodeAnalysis;
-using Vidly.WebApi.DataAccess.Contexts;
-using Vidly.WebApi.DataAccess.Repositories;
-using Vidly.WebApi.Services.Movies;
-using Vidly.WebApi.Services.Movies.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,20 +14,6 @@ builder
 
 var services = builder.Services;
 var configuration = builder.Configuration;
-
-var vidlyConnectionString = configuration.GetConnectionString("Vidly");
-
-if (string.IsNullOrEmpty(vidlyConnectionString))
-{
-    throw new Exception("Missing Vidly connection string");
-}
-
-services
-    .AddDbContext<DbContext, VidlyDbContext>(
-    options => options.UseSqlServer(vidlyConnectionString));
-
-services.AddScoped<IRepository<Movie>, Repository<Movie>>();
-services.AddScoped<IMovieService, MovieService>();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
