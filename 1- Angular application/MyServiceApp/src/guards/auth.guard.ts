d98564094dc7d9ce3ff8bf505b46fdb8665d90b9
Daniel Acevedo <daniel18acevedo@gmail.com>
2024-06-06 10:18:17 -0300

    Update auth.guard.ts

--- 1- Angular application/MyServiceApp/src/guards/auth.guard.ts
+++ 1- Angular application/MyServiceApp/src/guards/auth.guard.ts
@@ -2,9 +2,9 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') !== null;
+  const isNotLoggedIn = localStorage.getItem('isLoggedIn') === null;
 
-  if (!isLoggedIn) {
+  if (isNotLoggedIn) {
     const router = inject(Router);
 
     return router.parseUrl('/login');
